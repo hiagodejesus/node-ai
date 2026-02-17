4bc977724a256d8170ca557b5ddc6be927a37d86
Hiago Oliveira de Jesus <hiago.oliveira.de.jesus@gmail.com>
2026-02-17 10:17:12 -0400

    add zod

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import { z } from 'zod';
+import { zodResponseFormat } from 'openai/helpers/zod.mjs';
 
 dotenv.config();
 
@@ -12,18 +14,30 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+const schema = z.object({
+    produtos: z.array(z.string()),
+})
+
 app.post('/generate', async (req: Request, res: Response) => {
     try {
-        const completion = await openai.chat.completions.create({
+        const completion = await openai.chat.completions.parse({
             model: "gpt-4o-mini",
+            max_completion_tokens: 100,
+            response_format: zodResponseFormat(schema, "produtos_schema"),
             messages: [
+                {
+                    role: "developer",
+                    content: "You are a nutritionist. Recommend products for a diet!",
+                },
                 {
                     role: "user",
                     content: req.body.message
                 },
             ],
         });
-        res.json(completion.choices[0].message.content);
+
+        res.json(completion.choices[0].message.parsed?.produtos);
+
     } catch (error) {
         console.error('Error calling OpenAI API:', error);
         res.status(500).send('Error communicating with OpenAI');
